fd3a0debb97ed98c294d2181d94a1c62f06da0c2
Mikhail_Nikolaev <mikhail.nikolaev@wemaysell.com>
2019-10-02 13:20:48 +0300

    change series colors

--- src/main/java/temp/pifcharts/ConfigService.java
+++ src/main/java/temp/pifcharts/ConfigService.java
@@ -21,25 +21,24 @@ import static temp.pifcharts.DataDownloader.createMapper;
  */
 public class ConfigService {
     private static List<SeriesConfig> DEFAULT_CONFIG = Arrays.asList(
-            new SeriesConfig(new Color(0xFF, 0x55, 0x55), "43", "Dobrynya_Nikitich", false),
-            new SeriesConfig(new Color(0x55, 0x55, 0xFF), "641", "prirResource"),
-            new SeriesConfig(new Color(0x55, 0xFF, 0x55), "5247", "mosBirgaBrutto"),
+            new SeriesConfig(new Color(0xff5555), "43", "Dobrynya_Nikitich", false),
+            new SeriesConfig(new Color(0x0000c0), "641", "prirResource", true),
+            new SeriesConfig(new Color(0x5555ff), "5247", "mosBirgaBrutto", true),
 
-            new SeriesConfig(new Color(0xFF, 0x40, 0x40), "2050", "glob_dolg"),
-            new SeriesConfig(new Color(0xFF, 0xFF, 0x55), "3125", "euroobligation", false),
+            new SeriesConfig(new Color(0x55ff55), "2050", "glob_dolg", true),
+            new SeriesConfig(new Color(0xffff55), "3125", "euroobligation", false),
 
-//            new SeriesConfig(new Color(0x55, 0xFF, 0xFF), 4265", "arendn_bisnes", false),
-            new SeriesConfig(new Color(0x00, 0xC0, 0xC0), "1070", "act_upravlenie", false),
-            new SeriesConfig(new Color(0xff, 0xaf, 0xaf), "47", "Ilia_Muromec", false),
-            new SeriesConfig(new Color(0x80, 0x80, 0x80), "764", "america", false),
-            new SeriesConfig(new Color(0xc0, 0x00, 0x00), "45", "sbalansir"),
-            new SeriesConfig(new Color(0x00, 0x00, 0xC0), "957", "fin_sector"),
-            new SeriesConfig(new Color(0xC0, 0xC0, 0x00), "2225", "gold"),
-            new SeriesConfig(new Color(0x00, 0xC0, 0x00), "803", "potrebSector"),
-            new SeriesConfig(new Color(0xC0, 0x00, 0xC0), "2233", "globInternet"),
-            new SeriesConfig(new Color(0x55, 0xFF, 0xFF), "640", "telecomAndTech"),
-            new SeriesConfig(new Color(0x40, 0x40, 0x40), "366", "perspect"),
-            new SeriesConfig(new Color(0xFF, 0x55, 0xFF), "642", "electroenerg")
+            new SeriesConfig(new Color(0x00c0c0), "1070", "act_upravlenie", false),
+            new SeriesConfig(new Color(0xffafaf), "47", "Ilia_Muromec", false),
+            new SeriesConfig(new Color(0x808080), "764", "america", false),
+            new SeriesConfig(new Color(0x00c000), "45", "sbalansir", true),
+            new SeriesConfig(new Color(0xff55ff), "957", "fin_sector", true),
+            new SeriesConfig(new Color(0xc0c000), "2225", "gold", true),
+            new SeriesConfig(new Color(0xc00000), "803", "potrebSector", true),
+            new SeriesConfig(new Color(0xff4040), "2233", "globInternet", true),
+            new SeriesConfig(new Color(0xc000c0), "640", "telecomAndTech", true),
+            new SeriesConfig(new Color(0x404040), "366", "perspect", true),
+            new SeriesConfig(new Color(0x55ffff), "642", "electroenerg", true)
     );
     private final ObjectMapper objectMapper = createMapper();
 
